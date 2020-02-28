dabede5a81ed321f346dfc9da7a487c93e68db17
hechmik <hechmi.khaled1995@gmail.com>
2020-02-28 21:21:01 +0100

    Add feed articles parsing

--- src/main.py
+++ src/main.py
@@ -1,5 +1,22 @@
 import feed
+import scraper
+import json
+import time
 
 if __name__ == "__main__":
-    articles = feed.get_feeds_articles()
+
+    with open("config/websites.json", "r") as f:
+        website_infos = json.load(f)
+    articles_infos = feed.get_feeds_articles(website_infos)
+    articles = {}
+    for feed in articles_infos:
+        for title in feed.keys():
+            print(title)
+            source = feed[title]['source']
+            main_div_class = website_infos[source]['main_class']
+            article_url = feed[title]['url']
+            print("source: {}, main_div: {}, url: {}".format(source, main_div_class, article_url))
+            text = scraper.scrape_page(article_url, main_div_class)
+            articles[title] = text
+            time.sleep(2)
     print(articles)
