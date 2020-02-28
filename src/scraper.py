3869f5429c307dfb954a6d23a1b64778f7ff77f1
hechmik <hechmi.khaled1995@gmail.com>
2020-02-28 20:23:11 +0100

    Add code for scraping articles

---
+++ src/scraper.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+def scrape_page(link, article_class):
+    """
+    Return the entire article in the given link
+    Parameters
+    ----------
+    link : str
+        Article URL
+
+    article_class: str
+        CSS Class of the dive that contains all the article paragraphs.
+    """
+    page = requests.get(link) 
+    soup = BeautifulSoup(page.content, 'html.parser')
+    article_content= soup.body.find(class_=article_class)
+    article = []
+    for paragraph in article_content.find_all('p'):
+        sentence = paragraph.get_text()
+        if sentence != "":
+            article.append(paragraph.get_text())
+    return article
+    
