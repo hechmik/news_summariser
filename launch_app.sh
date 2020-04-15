39854f18b32ac7463d60aa8c238a23e31c705ba8
hechmik <hechmi.khaled1995@gmail.com>
2020-04-15 18:01:00 +0200

    Add scripts for building image and launch container

---
+++ launch_app.sh
@@ -0,0 +1,1 @@
+docker run --rm -v /Users/kappa/repositories/news_summariser/output_summaries:/news_summariser/output_summaries -v /Users/kappa/repositories/news_summariser/articles_db:/news_summariser/articles_db -v /Users/kappa/repositories/news_summariser/log:/news_summariser/log news_summariser
